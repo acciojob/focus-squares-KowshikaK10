8e4329d3f50439cee2e6c3527d3a0003626526b7
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-03 21:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+	const squares=document.querySelectorAll('.square');
+	squares.forEach((square)=>{
+		square.addEventListener('mouseover',(e)=>{
+			squares.forEach((sq)=>{
+				if(sq!=e.target){
+					sq.style.backgroundColor = "#6F4E37"
+				}
+			})
+		})
+		square.addEventListener('mouseout',(e)=>{
+			squares.forEach((sq)=>{
+				if(sq!=e.target){
+					sq.style.backgroundColor = "#E6E6FA"
+				}
+			})
+		})
+	})
+});
